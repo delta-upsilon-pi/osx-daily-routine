ad44d566f067e134ce78d7dceab422c0bd235e7f
Denis Y. Panin <denis.y.panin@gmail.com>
2018-02-12 22:06:51 +0200

    Minor things

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # OSX Daily Routine
 
-Don't forget execute `bash ./install.sh`
-
-* `march/siege` – turns MacBook into corresponding mode, enables/disables periphery and Wi-Fi
-
-* `report` – informs about given command finishing, uses system notification and voice synthesizer
+Personal tools for everyday routine automation
--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@ else
     if ! [ -x "$(command -v blueutil)" ]; then
         echo "Installing blueutil..."
         brew install blueutil
+        chmod +x ./march ./siege
+        cp ./{march,siege} /usr/local/bin
     fi
 fi
 
-chmod +x ./report ./march ./siege
-cp ./{report,march,siege} /usr/local/bin
+chmod +x ./report
+cp ./report /usr/local/bin
